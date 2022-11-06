acf828909ce0e9f61860e2abd838c6689c2b28d0
Theo Levequet <theo.leveque@gmail.com>
2022-11-05 22:13:22 -0700

    add find one by slug for recipe

--- src/api/recipe/controllers/recipe.ts
+++ src/api/recipe/controllers/recipe.ts
@@ -1,7 +1,27 @@
 /**
  * recipe controller
  */
+import { factories } from "@strapi/strapi";
 
-import { factories } from '@strapi/strapi'
+export default factories.createCoreController(
+  "api::recipe.recipe",
+  ({ strapi }) => ({
+    async findOne(ctx) {
+      const { slug } = ctx.params;
 
-export default factories.createCoreController('api::recipe.recipe');
+      const query = {
+        filters: { slug },
+        ...ctx.query,
+      };
+
+      const post = await strapi.entityService.findMany(
+        "api::recipe.recipe",
+        query
+      );
+
+      const sanitizedEntity = await this.sanitizeOutput(post);
+
+      return this.transformResponse(sanitizedEntity[0]);
+    },
+  })
+);
--- src/api/recipe/routes/recipe.ts
+++ src/api/recipe/routes/recipe.ts
@@ -1,7 +1,50 @@
 /**
  * recipe router
  */
+import { factories } from "@strapi/strapi/";
 
-import { factories } from "@strapi/strapi";
+const defaultRouter = factories.createCoreRouter("api::recipe.recipe");
 
-export default factories.createCoreRouter("api::recipe.recipe");
+// function to add to or override default router methods
+const customRouter = (innerRouter, routeOveride = [], extraRoutes = []) => {
+  let routes;
+
+  return {
+    get prefix() {
+      return innerRouter.prefix;
+    },
+    get routes() {
+      if (!routes) routes = innerRouter.routes;
+
+      const newRoutes = routes.map((route) => {
+        let found = false;
+
+        routeOveride.forEach((overide) => {
+          if (
+            route.handler === overide.handler &&
+            route.method === overide.method
+          ) {
+            found = overide;
+          }
+        });
+
+        return found || route;
+      });
+
+      return newRoutes.concat(extraRoutes);
+    },
+  };
+};
+
+// Overide the default router with the custom router to use slug.
+const myOverideRoutes = [
+  {
+    method: "GET",
+    path: "/recipes/:slug",
+    handler: "api::recipe.recipe.findOne",
+  },
+];
+
+const myExtraRoutes = [];
+
+module.exports = customRouter(defaultRouter, myOverideRoutes, myExtraRoutes);
